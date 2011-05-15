13e47d2a6355c767999d6642130ac9371c12f18d
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-15 14:33:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cool-fade-popup/trunk@385071 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,22 @@
+<h2><?php echo wp_specialchars( 'Plugin Management!' ); ?></h2>
+Click "Go to - Content Management" button to add/update/delete the popup content. <br />
+Click "Go to - Popup Setting" button to update popup setting.
+<h2><?php echo wp_specialchars( 'How to use!' ); ?></h2>
+<a target="_blank" href='http://www.gopiplus.com/work/2011/04/22/wordpress-plugin-wp-fadein-text-news/'>We have two option check this link</a>
+<h2><?php echo wp_specialchars( 'About Plugin!' ); ?></h2>
+Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>www.gopiplus.com</a>.<br />
+<a target="_blank" href='http://www.gopiplus.com/work/2011/04/22/wordpress-plugin-wp-fadein-text-news/'>Click here</a> to see more information, FAQ answer, Live demo, User comments<br />
+<a target="_blank" href='http://www.gopiplus.com/work/plugin-list/'>Click here</a> to download my other plugins.<br />
+<h2><?php echo wp_specialchars( 'Faq!' ); ?></h2>
+<ul>
+  <li>  How to enable popup on all the website pages?</li>
+  <li>  How to enable popup on particular page?</li>
+  <li>  What is group name in the form?</li>
+  <li>  Suppose if i have more then one content can we display that in random?</li>
+  <li>  Is possible to add HTML content?</li>
+  <li>  How to arrange the popup style?</li>
+  <li>  Can make this pop-up only for the first time visit ?</li>
+  <li>  My theme does not support widgets. How to enable popup on all the pages?</li>
+</ul>
+<br />
+
